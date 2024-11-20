909c3fa53425ff862a7a7b4a8dbce76490c32dc9
apollorion <joey@apollorion.com>
2024-11-20 18:08:07 -0500

    feat: dynamic readme

---
+++ .github/workflows/update_readme.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+
+gen_top(){
+  cat <<EOF > README.md
+![Personal Website](https://raw.githubusercontent.com/Apollorion/apollorion/main/logos/new-large-white-transparent.png#gh-dark-mode-only)![Personal Website](https://raw.githubusercontent.com/Apollorion/apollorion/main/logos/new-large-black-transparent.png#gh-light-mode-only)
+
+<p align="center">
+    <b>Hello, I'm Joey 👋</b>
+</p>
+
+EOF
+}
+
+gen_blusky_posts(){
+  # Query bluesky for posts
+  export MAX_POSTS=5
+  posts=$(curl -sS -H "Accept: application/json" "https://public.api.bsky.app/xrpc/app.bsky.feed.getAuthorFeed?actor=apollorion.com&limit=$MAX_POSTS")
+
+  length=$(echo "$posts" | jq ".feed | length")
+
+  # Parse the posts
+  for i in $(seq 0 $((length - 1))); do
+      text=$(echo "$posts" | jq -r ".feed[$i].post.record.text")
+      uri=$(echo "$posts" | jq -r ".feed[$i].post.uri" | awk -F'/' '{print $NF}')
+
+      date=$(echo "$posts" | jq -r ".feed[$i].post.record.createdAt")
+      date=${date%%T*}
+
+      echo "$date: [$text](https://bsky.app/profile/apollorion.com/post/$uri)  " >> README.md
+  done
+
+  echo "" >> README.md
+}
+
+gen_opentofu_resource(){
+
+  links=$(curl -sS https://raw.githubusercontent.com/Apollorion/profile/refs/heads/main/_data/links.yml)
+
+  cat <<EOF >> README.md
+\`\`\`hcl
+resource "github_introduction" "joey" {
+    name      = "Joey Stout"
+    interests = ["kubernetes", "opentofu", "nodejs", "python", "gitops"]
+    resume    = "https://apollorion.com/joeysResume.pdf"
+
+EOF
+
+  length_of_categories=$(echo "$links" | yq '.buttons | length')
+
+  for i in $(seq 0 $((length_of_categories - 1))); do
+    category=$(echo "$links" | yq -r ".buttons[$i].category" | tr '[:upper:]' '[:lower:]' | tr ' ' '_')
+    length_of_items=$(echo "$links" | yq ".buttons[$i].items | length")
+
+    echo "    $category = [" >> README.md
+
+    for j in $(seq 0 $((length_of_items - 1))); do
+      url=$(echo "$links" | yq -r ".buttons[$i].items[$j].url")
+      echo "        \"$url\"," >> README.md
+    done
+
+    echo "    ]" >> README.md
+    if [ $i -ne $((length_of_categories - 1)) ]; then
+      echo "" >> README.md
+    fi
+  done
+
+cat <<EOF >> README.md
+}
+\`\`\`
+
+EOF
+}
+
+gen_bottom(){
+  cat <<EOF >> README.md
+
+<p align="center">
+    <a href="https://www.buymeacoffee.com/apollorion"><sub><sub>Buy Me A Coffee</sub></sub></a> <sub><sub>|</sub></sub> <a href="https://apollorion.com/joeysResume.pdf"><sub><sub>Resume</sub></sub></a>
+</p>
+EOF
+}
+
+# Actually call the functions and generate the readme
+gen_top
+gen_opentofu_resource
+gen_blusky_posts
+gen_bottom
--- README.md
+++ README.md
@@ -10,20 +10,41 @@ resource "github_introduction" "joey" {
     interests = ["kubernetes", "opentofu", "nodejs", "python", "gitops"]
     resume    = "https://apollorion.com/joeysResume.pdf"
 
-    maintainer_of = [
-        "https://manifests.io/",
-        "https://fly.apollorion.com/",
-        "https://github.com/Apollorion/easy-k8s-secrets"
+    where_you_can_find_me = [
+        "https://www.github.com/apollorion",
+        "https://www.youtube.com/@TheOutdoorProgrammer",
+        "https://bsky.app/profile/apollorion.com",
+        "https://www.linkedin.com/in/apollorion/",
+        "mailto:joey@apollorion.com",
     ]
 
-    contact = {
-        email     = "joey@apollorion.com"
-        linkedin  = "https://www.linkedin.com/in/apollorion"
-        instagram = "https://www.instagram.com/sirjosephaaronstout/"
-        twitter   = "https://twitter.com/apollorion"
-    }
+    projects = [
+        "https://www.manifests.io/",
+        "https://www.wtc-the-game.org/",
+        "https://situationroom.apollorion.com/",
+        "https://github.com/Apollorion/fly",
+        "https://github.com/Apollorion/MAAS2",
+    ]
+
+    spacelift_modules = [
+        "https://github.com/Apollorion/MRPICKLE",
+        "https://github.com/spacelift-solutions/plugin-sops",
+        "https://github.com/spacelift-solutions/plugin-context-trigger",
+        "https://github.com/spacelift-solutions/plugin-signed-runs",
+    ]
+
+    blog_posts = [
+        "https://spacelift.io/blog/autoscaling-native-kubernetes-workers",
+        "https://spacelift.io/blog/best-practices-for-iac-and-spacelift",
+        "https://medium.com/beambenefits/collecting-open-metrics-in-k8s-and-shipping-to-datadog-5345cd27ef18",
+    ]
 }
 ```
+
+2024-11-20: [Is it a sin to link to X on here? Oh well, real cool Spacelift thing: x.com/k8sfm/status...](https://bsky.app/profile/apollorion.com/post/3lbfuxetscc2h)  
+2024-11-20: [I made my second post too, just in case.](https://bsky.app/profile/apollorion.com/post/3lbfp3dftsk27)  
+2024-11-20: [Mic check hello hello is this thing on? This is my first post here <3](https://bsky.app/profile/apollorion.com/post/3lbfnzsdybs2n)  
+
 
 <p align="center">
     <a href="https://www.buymeacoffee.com/apollorion"><sub><sub>Buy Me A Coffee</sub></sub></a> <sub><sub>|</sub></sub> <a href="https://apollorion.com/joeysResume.pdf"><sub><sub>Resume</sub></sub></a>
